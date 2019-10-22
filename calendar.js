80adda759986f24cd9944331df09b4db15212183
Catherine Lim <benglim@yahoo.com>
2019-10-21 20:53:17 -0700

    HW5 2nd commit

--- calendar.js
+++ calendar.js
@@ -1,143 +1,89 @@
-// var moment = require('moment');
-const dateFormat = 'YYYY-MM-dd';
-const octDate = '2019-10-14';
-const numberOfDays = 7;
+$(document).ready(function () {
 
-const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-const shortMonths = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-
-const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-const shortDays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-
-// date
-var currentTime = moment(octDate,dateFormat); // current date & time
-//currentTime = moment();
-// console.log(currentTime);
-
-// start of the month
-var startDate = currentTime.startOf('month');
-// week # at start of month
-var startWeek = startDate.week();
-
-// end of the month
-var endDate = currentTime.endOf('month');
-// week # at end of month
-var endWeek = endDate.week();
-
-//console.log(currentTime);
-
-// date: obtaining new instance of date & time
-currentTime = moment(octDate,dateFormat);
-//currentTime = moment();
-
-// console.log(currentTime);
-
-var totalNumberOfWeeks = endWeek - startWeek; // # of rows needed to render the current Month
-
-var monthHeaderString = shortMonths[currentTime.month()] + " "+currentTime.year();
-
-//appending the month as a div to body
-var monthHeader = document.createElement('DIV');
-monthHeader.setAttribute('class','monthHeader');
-monthHeader.innerText = monthHeaderString;
-
-document.getElementById('month').appendChild(monthHeader);
-
-var weekHeader = document.createElement("DIV");
-weekHeader.setAttribute("class","weekHeader");
-document.getElementById('month').appendChild(weekHeader);
-
-//adding day headers to weekHeader div
-for(var day=0;day<numberOfDays;day++){
-    // create a DOM div element
-    var dayOfWeek = document.createElement('DIV');
-    dayOfWeek.setAttribute('class','dayHeader');
-    dayOfWeek.innerText = shortDays[day];
-    //getElementsByClassName returns an array
-    document.getElementsByClassName('weekHeader')[0].appendChild(dayOfWeek);
-}
-//appending weeks to the month div
-for (var i = startWeek; i<=endWeek;i++){
-    var weekElement = document.createElement('DIV');
-    weekElement.setAttribute('class','week');
-    weekElement.setAttribute('id','week'+i);//week1 etc
-    document.getElementById('month').appendChild(weekElement);
-}
-
-// console.log('currentTime before cloning',currentTime);
-
-//date
-var startOfMonthLoop = moment(octDate,dateFormat).startOf('month');
-//currentTime = moment();
-// console.log('start of month:', startOfMonthLoop);
-
-var endOfMonthFlag = true;
-//populating each week with a day div. Final grid should be number of weeks x 7 days
-for(var i = startWeek; i<=endWeek;i++){
-    for(var day =0;day<numberOfDays;day++){ // day will cycle from 0 to 6
-        var dayElement = document.createElement('DIV');
-        dayElement.setAttribute('class','day');
-
-        if(day==0 || day==6){
-            dayElement.setAttribute('class','day weekend');
-        }
-        
-        if(i == startOfMonthLoop.week() && day==startOfMonthLoop.day() && endOfMonthFlag){
-            
-            if(startOfMonthLoop.date()==endDate.date()){
-                endOfMonthFlag=false;
-                }
-            console.log('start of month ',startOfMonthLoop.date(),'end date' ,endDate.date(),'current time: ',currentTime.date());
-            if( (startOfMonthLoop.date() == currentTime.date() ) && (day==0 || day==6) ){
-                dayElement.setAttribute('class','day weekend today');
-            } else if (startOfMonthLoop.date() == currentTime.date()) {
-                dayElement.classList.add('today');
-                dayElement.classList.add('day');
-            }
-            // div class=dayNumber (number header)
-            var dayNumberElement = document.createElement('DIV');
-            dayNumberElement.setAttribute('class','dayNumber');
-
-            dayNumberElement.innerText = startOfMonthLoop.date();
-            dayElement.appendChild(dayNumberElement);
-
-                //dayBody div class=dayBody
-            var dayBodyElement = document.createElement('DIV');
-            dayBodyElement.classList.add('dayBody');
-
-            var dayFooterElement = document.createElement('DIV');
-            dayFooterElement.classList.add('dayFooter');
-            var plusIcon = document.createElement('SPAN');
-            plusIcon.classList.add('fas');
-            plusIcon.classList.add('fa-calendar-plus');
-            plusIcon.classList.add('icon');
-            plusIcon.setAttribute('onclick','createEvent(this)')
-            dayFooterElement.appendChild(plusIcon);
-
-            dayElement.appendChild(dayBodyElement);
-            dayElement.appendChild(dayFooterElement);
-
-            startOfMonthLoop.add(1,'days');//increment by 1 day
-        } 
-
-        document.getElementById('week'+i).appendChild(dayElement);
-        
-
-    }
-}
-
-//appending days to each week div
-var firstNumericalDay = startDate.date(); // for mar: 1
-var firstDay = startDate.day(); 
-var lastNumericalDay = endDate.date(); // for mar: 31
-var lastDay = endDate.day(); 
-
-for(var i = firstNumericalDay; i<=lastNumericalDay;i++){
+    //add the current date to the #current date id in the HTML//
+    var dateString = moment().format("dddd, MMMM Do YYYY, h:mm a");
+    $("#currentDay").html(dateString);
     
-    //a check should be made to see what week we're in and 
-}
-
-
-function createEvent(element) {
-
-}
+    //create html timeblocks with for loop starting at 9 am until 5pm//
+    var scheduledHours = [];
+    
+    for (var hour = 9; hour < 18; hour++) {
+        scheduledHours.push(moment({
+            hour
+        }).format('h  a'));
+        $('.container').append(`<div class="row time-block" data-time=${hour}> 
+           <!--hour column-->
+               <div class="col-sm col-md-2 hour" data-time=${hour}> 
+                 <p class=dayHour>${moment({hour}).format('h  a')}</p>
+               </div> 
+               
+           <!--user input text area-->
+               <div class="col-sm col-md-8 d-flex description" data-time=${hour}> 
+                 <textarea class=textArea></textarea> 
+               </div> 
+          
+           <!--bootstrap saveBtn-->
+               <div class="col-sm col-md-2 saveBtn" data-time=${hour}>
+               <i class="far fa-save fa-2x" id=icon></i>  
+               </div>`);
+      //  console.log({
+      //      hour
+      //  });
+    }
+    
+    
+    //Check the hour of the current day to the hour represented in the HTML data-element to decide it's background color.
+    var m = moment();
+    $.each($(".time-block"), function (index, value) {
+        let dateHour = $(value).attr("data-time");
+        if (Number(dateHour) === m.hour()) {
+            $(this).find("textarea").addClass('present');
+        } else if (Number(dateHour) < m.hour()) {
+            $(this).find("textarea").addClass('past');
+        } else {
+            $(this).find("textarea").addClass('future');
+        }
+    });
+    
+    //Check for local storage to set value to the object
+    let timeObject = {};
+      if (localStorage.getItem('timeObject')) {
+          timeObject = JSON.parse(localStorage.getItem('timeObject'));
+      }else{
+        timeObject = {
+          '9': { time: "9", value: ""},
+          '10':{ time: "10", value: ""},
+          '11':{ time: "11", value: ""},
+          '12':{ time: "12", value: ""},
+          '13':{ time: "13", value: ""},
+          '14':{ time: "14", value: ""},
+          '15':{ time: "15", value: ""},
+          '16':{ time: "16", value: ""},
+          '17':{ time: "17", value: ""}
+        };
+      }
+    
+    //set value of timeObject to equal the user input for each row 
+    $(".description").each(function(){
+       $(this).find(".textArea").val(timeObject[$(this).attr("data-time")].value);
+       //console.log(this)
+      });
+    
+     //save value to local storage on click
+     $(document).on('click', '.saveBtn', function(event){
+       var data = $(this).attr("data-time") 
+        console.log('alert' + data);
+         //set timeObject time attribute
+         var timeValue = $(this).attr("data-time");
+        //set timeObject value attribute
+         var textValue = $(".time-block").find(".textArea").val();
+         timeObject[timeValue].value = textValue;
+    
+      //save user input in each object to local storage
+         localStorage.setItem('timeObject', JSON.stringify(timeObject));
+    
+          //console.log(textValue)
+    
+     });
+     
+    });
